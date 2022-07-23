c164ea627231020dd3873bfb5bd2fbececa720e9
Dawid Zbinski <dawid@zbinski.dev>
2022-07-23 18:49:03 +0200

    removed unused styles

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'ts-loader',
                 options: {
-                    getCustomTransformers: () => pathsTransformer()
+                    getCustomTransformers: () => pathsTransformer(),
                 },
             },
         ],
